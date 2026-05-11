21171bc3bfc85ede918ca77bddd83601f401eb02
Harshil Patel <98446850+harshil3134@users.noreply.github.com>
2026-05-11 22:08:33 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,35 @@
+class TimeMap:
+
+    def __init__(self):
+        self.kmap={}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.kmap.setdefault(key,[]).append([timestamp,value])
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        
+        klist=self.kmap.get(key,[])
+        if klist:
+            res=self.binarysearch(klist,0,len(klist)-1,timestamp) 
+            return klist[res][1] if klist[res][0]<=timestamp else ""
+        else:
+            return ""
+
+    def binarysearch(self,arr,start,end,target):
+
+        if start>end:
+            return end
+
+        mid=(start+end)//2
+        if target==arr[mid][0]:
+            return mid
+        elif arr[mid][0]<target:
+            return self.binarysearch(arr,mid+1,end,target)
+        else:
+            return self.binarysearch(arr,start,mid-1,target)
+        
+
+
+
+
